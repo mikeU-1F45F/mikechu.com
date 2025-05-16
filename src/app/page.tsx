2d66541d8ce7ab94921bad31a0d51fc6bf73d8fb
Mike Chu <michael.chu@optimizely.com>
2025-05-16 17:07:48 -0400

    chore: remove demo components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
 "use client";
 
-import { TypographyDemo } from "@/components/TypographyDemo";
-import { ColorPaletteDemo } from "@/components/ColorPaletteDemo";
-
 export default function Home() {
-
-
-    return (
-        <main className="container mx-auto">
-            <h1 className="text-3xl font-bold mb-4">Mike Chu Portfolio</h1>
-            <TypographyDemo />
-            <ColorPaletteDemo />
-        </main>
-    );
+    return <main className="container mx-auto"></main>;
 }
